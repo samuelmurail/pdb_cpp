36d2eec845b42e05d56786468c177f89a2fcfcbb
samuelmurail <samuel.murail@gmail.com>
2026-02-13 00:36:49 +0100

    Fix dependencies and c++ version

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
+import sys
+
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 import pybind11
+
+
+def _cpp_args():
+    if sys.platform == "win32":
+        return ["/std:c++17"]
+    return ["-std=c++17"]
 
 ext_modules = [
     Extension(
@@ -21,7 +29,7 @@ ext_modules = [
             pybind11.get_include(),
             "src/pdb_cpp",
         ],
-        extra_compile_args=["-O3", "-ffast-math"],
+        extra_compile_args=["-O3", "-ffast-math"] + _cpp_args(),
         language="c++"
     ),
 ]
